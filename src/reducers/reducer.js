52fbf5649c745f164d9d0a3d4f258b19f999fca9
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-03 11:04:41 -0700

    add failed test for edit item

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -7,7 +7,7 @@ export default (state = [], action) => {
     case 'DELETE_ITEM':
       return state.filter(item => item.name !== name);
     case 'EDIT_ITEM':
-      return state.map(item => item.name === name ? { name, description, available, price } : item)
+      return state.map(item => item.name === name ? { name, description, available, price } : item);
     default:
       return state;
   }
