ee48e4b44d3b0ad3430f853be2699cff17d9669a
Kent McKinney <kent.p.mckinney@gmail.com>
2020-05-03 14:58:40 -0700

    finished refactoring to redux

--- src/component/ItemList/ItemList.jsx
+++ src/component/ItemList/ItemList.jsx
@@ -19,14 +19,6 @@ class ItemList extends Component {
 
   }
 
-  handleClickShowDetail = event => {
-    this.setState({ showDetail: event.target.name });
-  }
-
-  handleClickBackFromDetail = event => {
-    this.setState({ showDetail: null });
-  }
-
   handleClickEdit = event => {
     this.setState({ editing: event.target.name });
   }
@@ -36,67 +28,59 @@ class ItemList extends Component {
   }
 
   handleClickDeleteItem = event => {
-    alert('delete');
+    this.props.dispatch({ type: 'DELETE_ITEM', key: event.target.id });
   }
 
-  handleClickSaveItem = (name, desc, avail, price, key) => {
+  handleClickSaveItem = (name, description, available, price, key) => {
     let data = this.props.data;
-    data = data.map(item => {
-      if (item.key === key) return { name: name, description: desc, available: avail, price: price, key: key }
-      else return item;
-    });
-    this.setState({ editing: null, data: data });
+    this.props.dispatch({ type: 'EDIT_ITEM', name, description, available, price, key });
+    this.setState({ editing: null });
+  }
+
+  generateNormalModeUI(item) {
+    return (
+      <React.Fragment>
+        <h3>{item.name}</h3>
+        <p><em>{item.description}</em></p>
+        <p>Available: {item.available} | Price: {item.price}</p>
+        <button onClick={this.handleClickEdit} name={item.key}>Edit</button>
+      </React.Fragment>
+    );
+  }
+
+  generateEditModeUI(item) {
+    return (
+      <React.Fragment>
+        <div><label htmlFor='itemname'>Name: <input id='itemname' defaultValue={item.name} /></label></div>
+        <div><label htmlFor='itemdesc'>Description: <input id='itemdesc' defaultValue={item.description}></input></label></div>
+        <div><label htmlFor='itemavail'>Available: <input id='itemavail' defaultValue={item.available}></input></label></div>
+        <div><label htmlFor='itemprice'>Price: <input id='itemprice' defaultValue={item.price}></input></label></div>
+        <br />
+        <div>
+          <button onClick={() => {
+            this.handleClickSaveItem(
+              document.getElementById('itemname').value,
+              document.getElementById('itemdesc').value,
+              document.getElementById('itemavail').value,
+              document.getElementById('itemprice').value,
+              item.key
+            )
+          }} name={item.key}>Save</button>
+          <button onClick={this.handleClickDeleteItem} name={item.key}>Delete Item</button>
+          <button onClick={this.handleClickCancelEdit} name={item.key}>Cancel</button>
+        </div>
+      </React.Fragment>
+    )
   }
 
   render() {
-    console.log(this.props.data)
     let ui = [];
     for (let i = 0; i < this.props.data.length; i++) {
-      if (this.props.data[i]) {
+      const item = this.props.data[i];
+      if (item) {
         ui.push(
-          <Item key={this.props.data[i].key}>
-            {this.state.editing && this.state.editing === this.props.data[i].key ?
-              /* EDIT MODE */
-              <React.Fragment>
-                <div><label htmlFor='itemname'>Name: <input id='itemname' defaultValue={this.props.data[i].name} /></label></div>
-                <div><label htmlFor='itemdesc'>Description: <input id='itemdesc' defaultValue={this.props.data[i].description}></input></label></div>
-                <div><label htmlFor='itemavail'>Available: <input id='itemavail' defaultValue={this.props.data[i].available}></input></label></div>
-                <div><label htmlFor='itemprice'>Price: <input id='itemprice' defaultValue={this.props.data[i].price}></input></label></div>
-                <br />
-                <div>
-                  <button onClick={() => {
-                    this.handleClickSaveItem(
-                      document.getElementById('itemname').value,
-                      document.getElementById('itemdesc').value,
-                      document.getElementById('itemavail').value,
-                      document.getElementById('itemprice').value,
-                      this.props.data[i].key
-                    )
-                  }} name={this.props.data[i].key}>Save</button>
-                  <button onClick={this.handleClickDeleteItem} name={this.props.data[i].key}>Delete Item</button>
-                  <button onClick={this.handleClickCancelEdit} name={this.props.data[i].key}>Cancel</button>
-                </div>
-              </React.Fragment>
-              :
-              this.state.showDetail && this.state.showDetail === this.props.data[i].key ?
-                /* DETAIL MODE */
-                <React.Fragment>
-                  <h1>Details</h1>
-                  <h3>{this.props.data[i].name}</h3>
-                  <p><em>{this.props.data[i].description}</em></p>
-                  <p>Available: {this.props.data[i].available} | Price: {this.props.data[i].price}</p>
-                  <button onClick={this.handleClickBackFromDetail} name={this.props.data[i].key}>Back</button>
-                </React.Fragment>
-                :
-                /* NORMAL MODE */
-                <React.Fragment>
-                  <h3>{this.props.data[i].name}</h3>
-                  <p><em>{this.props.data[i].description}</em></p>
-                  <p>Available: {this.props.data[i].available} | Price: {this.props.data[i].price}</p>
-                  <button onClick={this.handleClickEdit} name={this.props.data[i].key}>Edit</button>
-                  <button onClick={this.handleClickShowDetail} name={this.props.data[i].key}>Details</button>
-                </React.Fragment>
-            }
+          <Item key={item.key}>
+            {this.state.editing && this.state.editing === item.key ? this.generateEditModeUI(item) : this.generateNormalModeUI(item)}
           </Item>
         )
       }
